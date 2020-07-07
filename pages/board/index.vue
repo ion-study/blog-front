9b44a97d9728394d00709d6f2528cbb546f3a060
yujin song <uzini_@likelion.org>
2020-07-07 23:30:30 +0900

    axios delete,update test 코드 작성 (updateTest 메서드 참고)

--- pages/board/index.vue
+++ pages/board/index.vue
@@ -3,7 +3,7 @@
     <h1>게시글 리스트</h1>
     <div>
       <ul>
-        <li v-for="post in boardList" :key="post.id">[{{post.boardId}}] / {{post.userId}} / {{post.subject}} / {{post.createdDate}}<nLink :to="`board/view/${post.id}`"></nLink></li>
+        <li v-for="post in boardList" :key="post.boardId">[{{post.boardId}}] / {{post.userId}} / {{post.subject}} / {{post.createdDate}}<nLink :to="`board/view/${post.boardId}`"></nLink><button v-on:click="updateTest(post.boardId)">[삭제]</button></li>
       </ul>
     </div>
   </div>
@@ -17,6 +17,13 @@
       console.log(data)
       return {
         boardList: data
+      }
+    },
+    methods: {
+      async updateTest(id) {
+        //let data2 = await this.$axios({method: 'delete', url:`boards/${id}`}) // delete test
+        //let data2 = await this.$axios.$patch(`boards/${id}`, {"subject": "axios subject수정", "contents": "axios content수정"}); // update test
+        //console.log(data2)
       }
     }
   }
